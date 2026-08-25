a252adab83047f75baae86a16a158cfa1f2db6ed
SamulVanunu <furhan04ali04@gmail.com>
2026-08-25 12:43:53 +0300

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,8 @@
+{
+  "name": "Film Depom",
+  "description": "CloudStream statik eklenti deposu",
+  "manifestVersion": 1,
+  "pluginLists": [
+    "https://raw.githubusercontent.com/<SamuelVanunu>/film-depom/master/plugins.json"
+  ]
+}
